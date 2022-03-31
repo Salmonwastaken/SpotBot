495ebff41290128c5766aacf9e542e618898d4b5
Salmonwastaken <gerdingjeremy@gmail.com>
2022-03-31 22:35:47 +0200

    Fix more spacing, add documentation and remove unused function

--- index.js
+++ index.js
@@ -44,24 +44,25 @@ spotifyApi.authorizationCodeGrant(code).then(
 );
 
 // Create a new Discord client instance
-const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+const client = new Client({intents: [Intents.FLAGS.GUILDS,
+  Intents.FLAGS.GUILD_MESSAGES]});
 
 // For troubleshooting. Really doesn't need to be async but it is.
 client.once(`ready`, (async ()=>{
   console.log(`Ready`);
-  // So apparently Spotify tokens get revoked after 3600 seconds aka an hour.
-  // To work around this we retrieve a new token every 3500 seconds (roughly 58 minutes), using our current (and still valid) Access and Refresh token.
+  /* So apparently Spotify tokens get revoked after 3600 seconds aka an hour.
+  To work around this we retrieve a new token every 3500 seconds,
+  using our current (and still valid) Access and Refresh token. */
   setInterval(async () => {
-    // clientId, clientSecret and refreshToken has been set on the api object previous to this call.
     spotifyApi.refreshAccessToken().then(
-  		function(data) {
-    			console.log('The access token has been refreshed!');
-    		// Save the access token so that it's used in future calls
-    			spotifyApi.setAccessToken(data.body['access_token']);
-  		},
-  		function(err) {
-    			console.log('Could not refresh access token', err);
-  		},
+        function(data) {
+          console.log('The access token has been refreshed!');
+          // Save the access token so that it's used in future calls
+          spotifyApi.setAccessToken(data.body['access_token']);
+        },
+        function(err) {
+          console.log('Could not refresh access token', err);
+        },
     );
   }, 3500000);
 }));
@@ -76,7 +77,8 @@ client.on(`messageCreate`, async (msg) => {
       if (msg.content.includes('https://open.spotify.com/track/')) {
         // Extract the Spotify URI/ID so we can use it.
         const URIID = useRegex(msg.content);
-        // Remove all occurrence of a track to prevent duplicates. Pretty lazy, but it works.
+        /* Remove all occurrence of a track to prevent duplicates.
+        Pretty lazy, but it works. */
         const tracks = [{uri: `spotify:track:${URIID}`}];
         await spotifyApi.removeTracksFromPlaylist(steamedcatsID, tracks)
             .then(function(data) {
@@ -85,10 +87,12 @@ client.on(`messageCreate`, async (msg) => {
               console.log('Deleting tracks failed', err);
             });
         // Add the track
-        await spotifyApi.addTracksToPlaylist(steamedcatsID, [`spotify:track:${URIID}`])
+        await spotifyApi.addTracksToPlaylist(steamedcatsID,
+            [`spotify:track:${URIID}`])
             .then(function(data) {
               console.log('Added tracks to playlist!');
-              // Just to let the people know that we managed to add the spotify track
+              /* Just to let the people know that we managed
+              to add the spotify track */
               msg.react(`👀`);
             }, function(err) {
               console.log('Adding tracks failed!', err);
@@ -120,13 +124,13 @@ client.on(`unhandledReject`, console.log);
 
 client.login(token);
 
-// Function section
-
+/**
+ * Grabs the song ID from Spotify URLs
+ * @param {string} input The Spotify Track URL
+ * @param {const} regex The regex used to extract the Spotify Track ID
+ * @return {string} the Spotify track ID
+ */
 function useRegex(input) {
   const regex = /(?<=track\/)([^?\n]+)/i;
   return input.match(regex);
-}
-
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
 }
